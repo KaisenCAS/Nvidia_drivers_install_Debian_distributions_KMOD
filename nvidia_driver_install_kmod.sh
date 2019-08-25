4fef2fd2af641a34ad501b9bbc627617d768fb60
Kaisen <kevinchevreuillinux@gmail.com>
2019-08-25 22:32:26 +0200

    Update nvidia_driver_install_kmod.sh

--- nvidia_driver_install_kmod.sh
+++ nvidia_driver_install_kmod.sh
@@ -3,8 +3,8 @@
 echo 'This script install Nvidia drivers for all distributions based in Debian'
 
 #Add repository stretch-backports
-echo 'deb http://httpredir.debian.org/debian stretch-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list
-#Update repositories for applicate modifications in /etc/apt/sources.list
+echo 'deb http://httpredir.debian.org/debian stretch-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/nvidia.list
+#Update repositories for applicate modifications in /etc/apt/sources.list.d/
 sudo apt-get update
 #Linux headers install
 sudo apt-get install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') -y
@@ -15,7 +15,7 @@ sudo apt-get update
 #Install nvidia driver with stretch-backports
 sudo apt-get install -t stretch-backports nvidia-driver -y
 #Add repository non-free for Debian Stretch
-echo 'deb http://httpredir.debian.org/debian/ stretch main contrib non-free' | sudo tee -a /etc/apt/sources.list
+echo 'deb http://httpredir.debian.org/debian/ stretch main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/nvidia.list
 #Update repositories
 sudo apt-get update
 #Install linux-headers for non-free packets
