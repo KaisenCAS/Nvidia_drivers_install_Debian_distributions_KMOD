bc84c2f0cbed9fc18963f55e0d554057888a78cc
Kaisen <kaisen@localhost.localdomain>
2019-12-24 01:24:54 +0100

    Add checking root execution

--- nvidia_driver_install_kmod.sh
+++ nvidia_driver_install_kmod.sh
@@ -1,26 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+
+#Check root execution verification
+if [[ "$EUID" -ne 0 ]]; then
+	echo -e "Run this script as root"
+	exit 1
+fi
 
 echo 'This script install Nvidia drivers for all distributions based in Debian'
 
 #Add repository stretch-backports
-echo 'deb http://httpredir.debian.org/debian stretch-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/nvidia.list
+echo 'deb http://httpredir.debian.org/debian stretch-backports main contrib non-free' | tee -a /etc/apt/sources.list.d/nvidia.list
 #Update repositories for applicate modifications in /etc/apt/sources.list.d/
-sudo apt-get update
+apt-get update
 #Linux headers install
-sudo apt-get install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') -y
+apt-get install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') -y
 #Linux headers install with stretch backports
-sudo apt-get install -t stretch-backports linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') -y
+apt-get install -t stretch-backports linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') -y
 #Update repositories
-sudo apt-get update
+apt-get update
 #Install nvidia driver with stretch-backports
-sudo apt-get install -t stretch-backports nvidia-driver -y
+apt-get install -t stretch-backports nvidia-driver -y
 #Add repository non-free for Debian Stretch
-echo 'deb http://httpredir.debian.org/debian/ stretch main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/nvidia.list
+echo 'deb http://httpredir.debian.org/debian/ stretch main contrib non-free' | tee -a /etc/apt/sources.list.d/nvidia.list
 #Update repositories
-sudo apt-get update
+apt-get update
 #Install linux-headers for non-free packets
-sudo apt-get install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') nvidia-driver -y
+apt-get install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') nvidia-driver -y
 #Install libraries OpenGL and nvidia-driver
-sudo apt-get install libgl1-nvidia-glx -y
+apt-get install libgl1-nvidia-glx -y
 #Reboot after install for applicate modifications
-sudo reboot
+reboot
